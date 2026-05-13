ab5ebfcd5371c3ceea77949b008b6a758b67f8fc
Umadvi <aumadevi397@gmail.com>
2026-05-13 11:28:10 +0530

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,51 +5,54 @@ function Navbar() {
   return (
     <nav
       style={{
-        margin: "0px",
+        margin: 0,
         backgroundColor: "navy",
         display: "flex",
-        gap: "30px",
-        height: "70px",
         alignItems: "center",
-        justifyContent: "center",
-        fontWeight: "bold",
-        fontSize: "20px",
+        height: "70px",
         width: "100%",
+        padding: "0 20px",
+        position: "relative",
       }}
     >
 
-      {/* Logo */}
+      {/* LOGO - LEFT SIDE */}
       <img
         src="https://gratech.vercel.app/_next/static/media/logo-light.c8c9f0c9.svg"
         alt="logo"
         style={{
           width: "150px",
           height: "40px",
-          alignContent: "left",
-          marginRight: "100px",
-      
         }}
       />
 
-      <Link style={{ color: "white", textDecoration: "none" }} to="/">
-        Home
-      </Link>
+      {/* LINKS - CENTER */}
+      <div
+        style={{
+          position: "absolute",
+          left: "50%",
+          transform: "translateX(-50%)",
+          display: "flex",
+          gap: "30px",
+        }}
+      >
+        <Link style={{ color: "white", textDecoration: "none" }} to="/">
+          Home
+        </Link>
+        <Link style={{ color: "white", textDecoration: "none" }} to="/about">
+          About
+        </Link>
+        <Link style={{ color: "white", textDecoration: "none" }} to="/services">
+          Services
+        </Link>
+        <Link style={{ color: "white", textDecoration: "none" }} to="/blogs">
+          Blogs
+        </Link>
+        <Link style={{ color: "white", textDecoration: "none" }} to="/contact">
+          Contact
+        </Link>
+      </div>
 
-      <Link style={{ color: "white", textDecoration: "none" }} to="/about">
-        About
-      </Link>
-
-      <Link style={{ color: "white", textDecoration: "none" }} to="/services">
-        Services
-      </Link>
-
-      <Link style={{ color: "white", textDecoration: "none" }} to="/blogs">
-        Blogs
-      </Link>
-
-      <Link style={{ color: "white", textDecoration: "none" }} to="/contact">
-        Contact
-      </Link>
     </nav>
   );
 }
